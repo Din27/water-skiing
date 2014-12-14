f0793c6e4b8aa6a228fafff7906c775c1c6d6d7c
Din <xenon027@gmail.com>
2014-12-14 22:19:04 +0200

    Added getters and setters for slalom, tricks and jumps fields in PlayerModel

--- js/model/PlayerModel.js
+++ js/model/PlayerModel.js
@@ -3,9 +3,9 @@ $(function () {
 		defaults: {
 			name: '',
 			gender: '',
-			slalomResult: 0.0,
 			slalomTrackIndex: 0, // index of track from SlalomData.js
 			slalomBuoysIndex: 0, // index of buoys from SlalomData.js
+			slalomResult: 0.0,
 			slalomScore: 0.0,
 			tricksResult: 0,
 			tricksScore: 0.0,
@@ -19,7 +19,7 @@ $(function () {
 	        });
 		},
 
-        // todo setters and getters and validation for scores and results
+        // todo validation for scores and results
 		validate: function (attrs) {
 			if (!$.trim(attrs.name)) {
 				return 'Имя спортсмена не должно быть пустым';
@@ -43,7 +43,70 @@ $(function () {
 
 		getGender: function () {
 			return this.get('gender');
-		}
+		},
 
+		setSlalomTrackIndex: function (slalomTrackIndex) {
+			return this.set({slalomTrackIndex: slalomTrackIndex}, {validate: true});
+		},
+
+		getSlalomTrackIndex: function () {
+			return this.get('slalomTrackIndex');
+		},
+
+		setSlalomBuoysIndex: function (slalomBuoysIndex) {
+			return this.set({slalomBuoysIndex: slalomBuoysIndex}, {validate: true});
+		},
+
+		getSlalomBuoysIndex: function () {
+			return this.get('slalomBuoysIndex');
+		},
+
+		setSlalomResult: function (slalomResult) {
+			return this.set({slalomResult: slalomResult}, {validate: true});
+		},
+
+		getSlalomResult: function () {
+			return this.get('slalomResult');
+		},
+
+		setSlalomScore: function (slalomScore) {
+			return this.set({slalomScore: slalomScore}, {validate: true});
+		},
+
+		getSlalomScore: function () {
+			return this.get('slalomScore');
+		},
+
+		setTricksResult: function (tricksResult) {
+			return this.set({tricksResult: tricksResult}, {validate: true});
+		},
+
+		getTricksResult: function () {
+			return this.get('tricksResult');
+		},
+
+		setTricksScore: function (tricksScore) {
+			return this.set({tricksScore: tricksScore}, {validate: true});
+		},
+
+		getTricksScore: function () {
+			return this.get('tricksScore');
+		},
+
+		setJumpResult: function (jumpResult) {
+			return this.set({jumpResult: jumpResult}, {validate: true});
+		},
+
+		getJumpResult: function () {
+			return this.get('jumpResult');
+		},
+
+		setJumpScore: function (jumpScore) {
+			return this.set({jumpScore: jumpScore}, {validate: true});
+		},
+
+		getJumpScore: function () {
+			return this.get('jumpScore');
+		}
 	});
 });
