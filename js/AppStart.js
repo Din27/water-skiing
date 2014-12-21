70b06bba8a4d26a71cbf859ac5bc2dde4b78f911
Din <xenon027@gmail.com>
2014-12-21 18:53:03 +0200

    Versioning and cleaning local storage added

--- js/AppStart.js
+++ js/AppStart.js
@@ -1,21 +1,22 @@
 $(function () {
 	new App.Router();
-	Backbone.history.start(); // todo del
+
+	window.appVersion = 2;
 
 	window.competitions = new App.Collection.Competition();
 	window.competitions.fetch();
 	if (
 		window.competitions.length < 4 ||
-		(window.competitions.length > 0 && window.competitions.first().getTeams().length > 0 && _.isUndefined(window.competitions.first().getTeams().first().getPlayers().first().getColorIndex()))
+		(window.competitions.length > 0 && window.competitions.first().get('version') !== window.appVersion)
 	) {
 		var competition;
 		while (competition = window.competitions.first()) {
 			competition.dispose();
 		};
-		window.competitions.add(new App.Model.Competition({name: 'Все', tabName: 'mixed'}));
-		window.competitions.add(new App.Model.Competition({name: 'До 14', tabName: 'u14'}));
-		window.competitions.add(new App.Model.Competition({name: 'До 17', tabName: 'u17'}));
-		window.competitions.add(new App.Model.Competition({name: 'До 21', tabName: 'u21'}));
+		window.competitions.add(new App.Model.Competition({name: 'Все', tabName: 'mixed', version: window.appVersion}));
+		window.competitions.add(new App.Model.Competition({name: 'До 14', tabName: 'u14', version: window.appVersion}));
+		window.competitions.add(new App.Model.Competition({name: 'До 17', tabName: 'u17', version: window.appVersion}));
+		window.competitions.add(new App.Model.Competition({name: 'До 21', tabName: 'u21', version: window.appVersion}));
 	};
 
 	var competitionsView = new App.View.CompetitionCollection({collection: competitions});
