f76de81ba7ba2b1293613a753832ba00775ff233
Din <xenon027@gmail.com>
2015-04-27 17:30:14 +0300

    Different competition types: Added possibility to change records

--- js/view/CompetitionView.js
+++ js/view/CompetitionView.js
@@ -11,7 +11,25 @@ $(function () {
 
 		template: template('competitionTemplate'),
 
-		_selectors: {},
+		_selectors: {
+			recordSlalomMen: '.js-world-record-slalom-men',
+			recordTricksMen: '.js-world-record-tricks-men',
+			recordJumpMen: '.js-world-record-jump-men',
+			recordSlalomWomen: '.js-world-record-slalom-women',
+			recordTricksWomen: '.js-world-record-tricks-women',
+			recordJumpWomen: '.js-world-record-jump-women'
+		},
+
+		events: function () {
+			var _events = {};
+			_events['change ' + this._selectors.recordSlalomMen] = 'editRecordSlalomMen';
+			_events['change ' + this._selectors.recordTricksMen] = 'editRecordTricksMen';
+			_events['change ' + this._selectors.recordJumpMen] = 'editRecordJumpMen';
+			_events['change ' + this._selectors.recordSlalomWomen] = 'editRecordSlalomWomen';
+			_events['change ' + this._selectors.recordTricksWomen] = 'editRecordTricksWomen';
+			_events['change ' + this._selectors.recordJumpWomen] = 'editRecordJumpWomen';
+			return _events;
+		},
 
 		render: function () {
 			this.$el.html(this.template(this.model.toJSON()));
@@ -34,6 +52,42 @@ $(function () {
 			this.$el.empty();
 			this.unbind();
 			return this;
+		},
+
+		editRecordSlalomMen: function () {
+			var recordSlalomMen = +$(this.el).find(this._selectors.recordSlalomMen).val();
+			this.model.setWorldRecordSlalomMen(recordSlalomMen);
+			this.render();
+		},
+		
+		editRecordTricksMen: function () {
+			var recordTricksMen = +$(this.el).find(this._selectors.recordTricksMen).val();
+			this.model.setWorldRecordTricksMen(recordTricksMen);
+			this.render();
+		},
+		
+		editRecordJumpMen: function () {
+			var recordJumpMen = +$(this.el).find(this._selectors.recordJumpMen).val();
+			this.model.setWorldRecordJumpMen(recordJumpMen);
+			this.render();
+		},
+		
+		editRecordSlalomWomen:function () {
+			var recordSlalomWomen = +$(this.el).find(this._selectors.recordSlalomWomen).val();
+			this.model.setWorldRecordSlalomWomen(recordSlalomWomen);
+			this.render();
+		},
+		
+		editRecordTricksWomen:function () {
+			var recordTricksWomen = +$(this.el).find(this._selectors.recordTricksWomen).val();
+			this.model.setWorldRecordTricksWomen(recordTricksWomen);
+			this.render();
+		},
+		
+		editRecordJumpWomen:function () {
+			var recordJumpWomen = +$(this.el).find(this._selectors.recordJumpWomen).val();
+			this.model.setWorldRecordJumpWomen(recordJumpWomen);
+			this.render();
 		},
 
 		destroy: function () {
