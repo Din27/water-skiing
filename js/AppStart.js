53eceb8f878b46434b4d3f834e5cc0643cce830d
Din <xenon027@gmail.com>
2015-04-29 04:10:18 +0300

    Icrement

--- js/AppStart.js
+++ js/AppStart.js
@@ -1,7 +1,7 @@
 $(function () {
 	new App.Router();
 
-	window.APP_VERSION = 22;
+	window.APP_VERSION = 23;
 	window.APP_LOCAL_STORAGE_VERSION_KEY = 'water-skiing-version';
 
 	var localStorageAppVersion = +localStorage.getItem(APP_LOCAL_STORAGE_VERSION_KEY);
